1e1e81f403b4fb7b51524868dbb6224e996a2d0e
Irina Lobova <lobovais@gmail.com>
2016-06-14 13:46:15 -0400

    user login and detecting auth events

--- src/js/services/authentication.js
+++ src/js/services/authentication.js
@@ -1,12 +1,30 @@
-regApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL',
-	function($rootScope, $firebaseAuth, FIREBASE_URL){
+regApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$firebaseObject','$location', 'FIREBASE_URL',
+	function($rootScope, $firebaseAuth, $firebaseObject, $location, FIREBASE_URL){
 
 		var ref = new Firebase(FIREBASE_URL);
 		var auth = $firebaseAuth(ref);
 
+		auth.$onAuth(function(authUser){
+			if(authUser){
+				var userRef = new Firebase(FIREBASE_URL + 'users/' + authUser.uid);
+				var userObj = $firebaseObject(userRef);
+				$rootScope.currentUser = userObj;
+				console.log(userObj.uid); 
+			} else {
+				$rootScope.currentUser = ''; 
+			}
+		});
+
 		return {
 			login: function(user){
-			    $rootScope.message = "Welcome " + user.email;
+				auth.$authWithPassword({
+					email: user.email,
+					password: user.password
+				}).then(function(regUser){
+					$location.path('/success');
+				}).catch(function(error){
+					$rootScope.message = error.message;
+				});
 			},//login
 
 			register: function(user){
