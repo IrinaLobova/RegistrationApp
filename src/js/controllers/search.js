4c6fa8f2d7d521ac6446b881542108e1aa5f241c
Irina Lobova <lobovais@gmail.com>
2016-10-06 21:28:18 +0300

    search factory

--- src/js/controllers/search.js
+++ src/js/controllers/search.js
@@ -1,3 +1,54 @@
+regApp.factory('searchService', ['$firebaseObject','$location', 'FIREBASE_URL', 
+	function( $firebaseObject, $location, FIREBASE_URL){
+		var results = [];
+		function search(q) {
+			var productsRef = new Firebase(FIREBASE_URL + 'products');
+			var query = productsRef.orderByChild('brand');
+
+			query.once("value", function(snapshot) {
+                snapshot.forEach(function(data) {
+                        var i = 0;
+                        if (data.key().startsWith(q)) {
+                                var values = data.val();
+                                var result = new Object();
+                                result.fullname = values.fullname;
+                                result.category = values.cathegory;
+                                var new_results = results.push(result);
+                                i++;
+                        }
+                });
+                
+				angular.copy(new_results, results);
+				console.log(results);
+			}, function(errors) {
+				console.log("The read failed: " + errors.code)
+			});
+		}
+		return {
+			results: results,
+			search: search
+		};
+}]);
+
+regApp.controller('SearchController', ['$scope', '$firebaseObject','$location', 'FIREBASE_URL',
+function($scope, $firebaseObject, $location, FIREBASE_URL) {
+	//$scope.q = '';
+	$scope.searchService = searchService;
+    $scope.searchService.results = searchService.results;
+    console.log($scope.searchService.results);
+
+    $scope.doSearch = function () {
+        $scope.searchService.search($scope.q);
+    };
+}])
+
+
+
+
+
+
+/*
+
 regApp.controller('SearchController', ['$scope', '$firebaseObject','$location', 'FIREBASE_URL',
 function($scope, $firebaseObject, $location, FIREBASE_URL) {
 
@@ -14,7 +65,8 @@ function($scope, $firebaseObject, $location, FIREBASE_URL) {
                                 var result = new Object();
                                 result.fullname = values.fullname;
                                 result.category = values.cathegory;
-                                $scope.results.push(result);
+                                $scope.newresults = $scope.results.push(result);
+								//angular.copy($scope.newresults, $scope.results)
                                 i++;
                         }
                 });
@@ -24,3 +76,10 @@ function($scope, $firebaseObject, $location, FIREBASE_URL) {
 			});
 	}
 }])
+
+
+
+
+
+
+
